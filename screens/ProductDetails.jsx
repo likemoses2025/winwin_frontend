0c46bfd831a3b452f0d19d9d9cc47f4fc86a7b6a
likemoses2025 <mose233@gmail.com>
2023-04-22 11:21:16 +0900

    Building Product Detail

--- screens/ProductDetails.jsx
+++ screens/ProductDetails.jsx
@@ -1,9 +1,30 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Dimensions, Image, StyleSheet } from "react-native";
+import React, { useRef } from "react";
 import { colors, defaultStyle } from "../styles/styles";
 import Header from "../components/Header";
+import Carousel from "react-native-snap-carousel";
+
+const SLIDER_WIDTH = Dimensions.get("window").width;
+const ITEM_WIDTH = SLIDER_WIDTH;
 
 const ProductDetails = ({ route: { params } }) => {
+  const isCarousel = useRef(null);
+
+  const images = [
+    {
+      id: "dklfjdlsf1",
+      url: "https://res.cloudinary.com/moses23/image/upload/v1681037643/dyyhx82ruo0gueoefrgp.png",
+    },
+    {
+      id: "dklfjdlsf2",
+      url: "https://res.cloudinary.com/moses23/image/upload/v1681038939/dscr5jhdy6agyveptjhy.png",
+    },
+    {
+      id: "dklfjdlsf3",
+      url: "https://res.cloudinary.com/moses23/image/upload/v1681038939/dscr5jhdy6agyveptjhy.png",
+    },
+  ];
+
   return (
     <View
       style={{ ...defaultStyle, padding: 0, backgroundColor: colors.color_s1 }}
@@ -11,8 +32,32 @@ const ProductDetails = ({ route: { params } }) => {
       <Header back={true} />
 
       {/* Carousel */}
+      <Carousel
+        layout="stack"
+        sliderWidth={SLIDER_WIDTH}
+        itemWidth={ITEM_WIDTH}
+        ref={isCarousel}
+        data={images}
+        renderItem={CarouselCardItem}
+      />
     </View>
   );
 };
+
+const CarouselCardItem = ({ item, index }) => (
+  <View style={styles.container} key={index}>
+    <Image source={{ uri: item.url }} style={styles.image} />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.color_s1,
+    width: ITEM_WIDTH,
+    paddingVertical: 60,
+    height: 380,
+  },
+  image: { width: ITEM_WIDTH, resizeMode: "contain", height: 250 },
+});
 
 export default ProductDetails;
