84158f42c89afbb9214562a0aa7dcf97e0cb649d
likemoses2025 <mose233@gmail.com>
2023-06-10 18:36:16 +0900

    Building Refund Page

--- components/RefundItem.jsx
+++ components/RefundItem.jsx
@@ -9,32 +9,56 @@ const RefundItem = ({}) => {
   return (
     <View
       style={{
+        marginTop: 10,
         backgroundColor: "white",
         elevation: 5,
         flexDirection: "row",
         alignItems: "center",
+        justifyContent: "center",
+        alignSelf: "center",
         padding: 5,
         borderRadius: 7,
+        paddingHorizontal: 20,
+        flex: 1,
       }}
     >
       <Text
         style={{
-          backgroundColor: colors.color1,
+          backgroundColor: colors.color_s3,
           width: 40,
           height: 40,
           borderRadius: 7,
           textAlign: "center",
-          justifyContent: "center",
-          alignSelf: "center",
-          alignItems: "center",
+          textAlignVertical: "center",
+          overflow: "hidden",
+          color: "white",
+          fontWeight: "900",
         }}
       >
         1
       </Text>
 
-      <Text>22년 7월</Text>
-      <Text>176박스</Text>
-      <View style={{ flexDirection: "row" }}>
+      <Text
+        style={{
+          textAlign: "center",
+          textAlignVertical: "center",
+          justifyContent: "center",
+          flex: 2,
+        }}
+      >
+        22년 7월
+      </Text>
+      <Text
+        style={{
+          textAlign: "center",
+          textAlignVertical: "center",
+          justifyContent: "center",
+          flex: 2,
+        }}
+      >
+        176박스
+      </Text>
+      <View style={{ flexDirection: "row", flex: 3 }}>
         <IconButton
           icon="update"
           iconColor={MD3Colors.primary0}
--- screens/Refund/Refunds.jsx
+++ screens/Refund/Refunds.jsx
@@ -1,6 +1,12 @@
 import { useIsFocused } from "@react-navigation/native";
 import React from "react";
-import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import {
+  Platform,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { Avatar, Headline } from "react-native-paper";
 import { useDispatch, useSelector } from "react-redux";
 import Header from "../../components/Header";
@@ -38,6 +44,7 @@ const Refunds = ({ navigation }) => {
         style={{
           marginBottom: 20,
           paddingTop: 70,
+          marginTop: Platform.OS === "ios" && 20,
         }}
       >
         <Text style={formHeading}>반 품 내 역</Text>
